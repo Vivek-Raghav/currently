bbb230006e3d8a4f1fa5250a44dcf7b9201c0418
Vivek-Raghav <vivekraghva@gmail.com>
2023-07-20 16:08:56 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/iconic/socialv/MainActivity.kt
@@ -0,0 +1,6 @@
+package ccom.currently.plusone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() {
+}
---
+++ lib/configs.dart
@@ -0,0 +1,46 @@
+/// App Name
+const APP_NAME = "Currently";
+
+/// App Icon src
+const APP_ICON = "assets/app_icon.png";
+
+/// Splash screen image src
+const SPLASH_SCREEN_IMAGE = 'assets/images/splash_image.png';
+
+/// OneSignal Notification App Id
+const ONESIGNAL_APP_ID = '';
+
+/// Todo: Remove Base URL
+/// NOTE: Do not add slash (/) at the end of your domain.
+const DOMAIN_URL = 'http://13.234.224.69/';
+const BASE_URL = '$DOMAIN_URL/wp-json/';
+
+/// AppStore Url
+const IOS_APP_LINK = 'https://apps.apple.com/us/app/socialv/id1641646237';
+
+/// Terms and Conditions URL
+const TERMS_AND_CONDITIONS_URL = '$DOMAIN_URL/terms-condition/';
+
+/// Privacy Policy URL
+const PRIVACY_POLICY_URL = '$DOMAIN_URL/privacy-policy-2/';
+
+/// Support URL
+const SUPPORT_URL = 'https://iqonic.desky.support';
+
+/// AdMod Id
+// Android
+const mAdMobAppId = '';
+const mAdMobBannerId = '';
+
+// iOS
+const mAdMobAppIdIOS = '';
+const mAdMobBannerIdIOS = '';
+
+const mTestAdMobBannerId = 'ca-app-pub-3940256099942544/6300978111';
+
+/// Woo Commerce keys
+
+// live
+const CONSUMER_KEY = 'ck_6951c2453a9d49cf00aad4c6519486a45b456a08';
+const CONSUMER_SECRET = 'cs_b97abf0bf1dc372d21d2036a81cd7ada9b672bdc';
+
